e1266c09fdb9221120fc18c9d3b3355964b88ca9
uanscarvalho <54337203+uanscarvalho@users.noreply.github.com>
2020-03-05 21:59:00 -0300

    Alteracoes

---
+++ src/main/java/br/com/uanscarvalho/bean/Response.java
@@ -0,0 +1,15 @@
+package br.com.uanscarvalho.bean;
+
+public class Response {
+	
+	private String mensagem;
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+
+}
--- src/main/java/br/com/uanscarvalho/controller/HelloworldController.java
+++ src/main/java/br/com/uanscarvalho/controller/HelloworldController.java
@@ -4,15 +4,21 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.uanscarvalho.bean.Pessoa;
+import br.com.uanscarvalho.bean.Response;
 
 @RestController
 public class HelloworldController {
-	
+
 	@RequestMapping(value = "/hello", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
-	public String welcome(@RequestBody Pessoa pessoa) {
-		return "Ola " + pessoa.getNome();
+	public @ResponseBody Response welcome(@RequestBody Pessoa pessoa) {
+
+		Response response = new Response();
+		response.setMensagem("Ola " + pessoa.getNome());
+
+		return response;
 	}
 }
